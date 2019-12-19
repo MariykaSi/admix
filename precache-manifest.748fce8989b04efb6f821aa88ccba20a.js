55d3527f23f199a2a239307515cd5110ef69a4aa
Mariyka Sinoverska <msinoverska@smartspaceplc.com>
2019-12-19 21:54:29 +0200

    Updates

---
+++ precache-manifest.748fce8989b04efb6f821aa88ccba20a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eeb5a3965515d239c923ab52e9aed6d",
+    "url": "/admixProject/index.html"
+  },
+  {
+    "revision": "3ab292ed0029d372f68c",
+    "url": "/admixProject/static/css/main.11fdd35a.chunk.css"
+  },
+  {
+    "revision": "e4ac9ad232f835faf070",
+    "url": "/admixProject/static/js/2.4189896a.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/admixProject/static/js/2.4189896a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ab292ed0029d372f68c",
+    "url": "/admixProject/static/js/main.61d33e0e.chunk.js"
+  },
+  {
+    "revision": "ca5bc37fe12f920e677b",
+    "url": "/admixProject/static/js/runtime-main.5061db8a.js"
+  },
+  {
+    "revision": "865591b1d309b6bca2af1146fe406d1c",
+    "url": "/admixProject/static/media/logo.865591b1.png"
+  }
+]);
