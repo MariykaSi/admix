5508d133cd5273a070b22ce13b9f549f32040406
Mariyka Sinoverska <msinoverska@smartspaceplc.com>
2019-12-19 22:55:16 +0200

    Updates

---
+++ precache-manifest.7cadf063abe3544000366b3208301d73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb5737c10ba5b471d1b0340e49e9f29d",
+    "url": "/admix-project/index.html"
+  },
+  {
+    "revision": "625b9a2427a44a51eab3",
+    "url": "/admix-project/static/css/main.11fdd35a.chunk.css"
+  },
+  {
+    "revision": "e4ac9ad232f835faf070",
+    "url": "/admix-project/static/js/2.4189896a.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/admix-project/static/js/2.4189896a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "625b9a2427a44a51eab3",
+    "url": "/admix-project/static/js/main.5236aa33.chunk.js"
+  },
+  {
+    "revision": "aedac9925b2e5309f1cb",
+    "url": "/admix-project/static/js/runtime-main.0a82c7e6.js"
+  },
+  {
+    "revision": "865591b1d309b6bca2af1146fe406d1c",
+    "url": "/admix-project/static/media/logo.865591b1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/admixProject/precache-manifest.748fce8989b04efb6f821aa88ccba20a.js"
+  "/admix-project/precache-manifest.7cadf063abe3544000366b3208301d73.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/admixProject/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/admix-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
